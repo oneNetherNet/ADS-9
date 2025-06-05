6df54db2bcdbb3dbb1836be87d3090ddded6c667
netherNet <whit3ufo@gmail.com>
2025-06-05 21:27:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,3 +5,28 @@
 #include  <cstdlib>
 #include  "tree.h"
 
+
+
+std::vector<std::vector<char>> getAllPerms(const Tree& tree) {
+  std::vector<std::vector<char>> out;
+  std::vector<char> buffer;
+  tree.gap_helper(tree.getRoot(), buffer, out);
+  return out;
+}
+
+std::vector<char> getPerm1(Tree& tree, int num)
+{
+  if (tree.fct() < num)
+    return std::vector<char>();
+  std::vector<std::vector<char>> allPerms = getAllPerms(tree);
+  return allPerms[num - 1];
+}
+
+std::vector<char> getPerm2(Tree& tree, int num)
+{
+  if (tree.fct() < num)
+    return std::vector<char>();
+  std::vector<char> out;
+  tree.gp2_helper(tree.getRoot(), num, out);
+  return out;
+}
